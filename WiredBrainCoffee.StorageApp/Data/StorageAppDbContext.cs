810791f90f6b0008a423321c988acd899d66644f
Raul.Laredo <Raul.Laredo@niceincontact.com>
2021-11-25 12:32:12 -0400

    Add project files.

---
+++ WiredBrainCoffee.StorageApp/Data/StorageAppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WiredBrainCoffee.StorageApp.Entities;
+
+namespace WiredBrainCoffee.StorageApp.Data
+{
+    public class StorageAppDbContext : DbContext
+    {
+        public DbSet<Employee>? Employees => Set<Employee>();
+        public DbSet<Organization>? Organizations => Set<Organization>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseInMemoryDatabase("StorageAppDb");
+        }
+    }
+}
---
+++ WiredBrainCoffee.StorageApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using WiredBrainCoffee.StorageApp.Data;
+using WiredBrainCoffee.StorageApp.Entities;
+using WiredBrainCoffee.StorageApp.Repositories;
+
+namespace WiredBrainCoffee.StorageApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var employeeRepository = new SqlRepository<Employee>(new StorageAppDbContext());
+      AddEmployees(employeeRepository);
+      GetEmployeeById(employeeRepository);
+
+      var organizationRepository = new ListRepository<Organization>();
+      AddOrganizations(organizationRepository);
+
+      Console.ReadLine();
+    }
+
+    private static void GetEmployeeById(ListRepository<Employee> employeeRepository)
+    {
+      var employee = employeeRepository.GetById(2);
+      Console.WriteLine($"Employee with Id 2: {employee.FirstName}");
+    }
+
+    private static void AddEmployees(ListRepository<Employee> employeeRepository)
+    {
+      employeeRepository.Add(new Employee { FirstName = "Julia" });
+      employeeRepository.Add(new Employee { FirstName = "Anna" });
+      employeeRepository.Add(new Employee { FirstName = "Thomas" });
+      employeeRepository.Save();
+    }
+
+    private static void AddOrganizations(ListRepository<Organization> organizationRepository)
+    {
+      organizationRepository.Add(new Organization { Name = "Pluralsight" });
+      organizationRepository.Add(new Organization { Name = "Globomantics" });
+      organizationRepository.Save();
+    }
+  }
+}
